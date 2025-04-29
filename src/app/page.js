628c1095312229d6cd98e9258a73a961fdf53683
RaiyanMFZ <sadgidmahfuz@gmail.com>
2025-04-29 15:42:06 +0200

    hero componente hinzugefügt

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { Hero1 } from "@/components/Hero1";
+import { Hero1 } from "@/components/ui/hero-with-text-and-two-button";
 import { useUser } from "@stackframe/stack";
 
 
--- src/components/ui/hero-with-text-and-two-button.jsx
+++ src/components/ui/hero-with-text-and-two-button.jsx
@@ -7,15 +7,10 @@ function Hero1() {
       <div className="container mx-auto">
         <div
           className="flex gap-8 py-20 lg:py-40 items-center justify-center flex-col">
-          <div>
-            <Button variant="secondary" size="sm" className="gap-4">
-              Read our launch article <MoveRight className="w-4 h-4" />
-            </Button>
-          </div>
           <div className="flex gap-4 flex-col">
             <h1
               className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
-              This is the start of something new
+              Make studying easier with us.
             </h1>
             <p
               className="text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center">
@@ -25,9 +20,6 @@ function Hero1() {
             </p>
           </div>
           <div className="flex flex-row gap-3">
-            <Button size="lg" className="gap-4" variant="outline">
-              Jump on a call <PhoneCall className="w-4 h-4" />
-            </Button>
             <Button size="lg" className="gap-4">
               Sign up here <MoveRight className="w-4 h-4" />
             </Button>
