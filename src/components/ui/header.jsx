74018e0d3b2462f4c5808f1c136763b8282adc89
Hari-Gian <harigian545@gmail.com>
2025-04-30 11:06:03 +0200

    header überarbeitet

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -4,19 +4,19 @@ import { Button } from '@/components/ui/button';
 
 export default function Header() {
   return (
-    <header className="bg-white shadow-xxs py-4 px-6 flex justify-between items-center">
-      <h1 className="text-2xl font-bold text-blue-600">MyApp</h1>
-      <nav className="space-x-6 items-center justify-center ml-20">
-        <Link href="/" className="text-gray-700 hover:text-blue-600">Home</Link>
-        <Link href="/about" className="text-gray-700 hover:text-blue-600">About</Link>
-        <Link href="/contact" className="text-gray-700 hover:text-blue-600">Contact</Link>
-      </nav>
-      <nav className='flex items-center'>
-      <Button size="lg" className="mr-4">
-        Create + 
-      </Button>
-      <UserButton />
-      </nav>
-    </header>
+      <header className="bg-white shadow-xxs py-4 px-6 flex justify-between items-center border-b border-gray-700 fixed top-0 left-0 w-full z-50">
+          <h1 className="text-2xl font-bold text-blue-600">MyApp</h1>
+          <nav className="space-x-10 items-center justify-center ml-20 text-lg">
+              <Link href="/" className="text-blue-700 hover:text-white">Home</Link>
+              <Link href="/about" className="text-blue-700 hover:text-white">About</Link>
+              <Link href="/contact" className="text-blue-700 hover:text-white">Contact</Link>
+          </nav>
+          <nav className='flex items-center'>
+              <Button size="lg" className="mr-4">
+                  Create +
+              </Button>
+              <UserButton/>
+          </nav>
+      </header>
   );
 }
