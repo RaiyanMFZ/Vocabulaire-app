c819a0feb6ba67551ede0af3c4363b5d2fbf26c0
RaiyanMFZ <sadgidmahfuz@gmail.com>
2025-05-02 08:19:17 +0200

    routing erstellt

---
+++ src/app/create/page.js
@@ -0,0 +1,7 @@
+export default function createPage() {
+    return (
+        <div className="mt-40">
+            <h1>Hello World</h1>
+        </div>
+    )
+}
--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -3,20 +3,22 @@ import { UserButton } from '@stackframe/stack';
 import { Button } from '@/components/ui/button';
 
 export default function Header() {
-  return (
-      <header className="bg-background shadow-xxs py-4 px-6 flex justify-between items-center border-b border-gray-700 fixed top-0 left-0 w-full z-50">
-          <h1 className="text-2xl font-bold text-blue-600">MyApp</h1>
-          <nav className="space-x-10 items-center justify-center ml-20 text-lg">
-              <Link href="/" className="text-blue-600 hover:text-blue-900 hover:text-bold">Home</Link>
-              <Link href="/about" className="text-blue-600 hover:text-blue-900 hover:text-bold">About</Link>
-              <Link href="/contact" className="text-blue-600 hover:text-blue-900 hover:text-bold">Contact</Link>
-          </nav>
-          <nav className='flex items-center'>
-              <Button size="lg" className="mr-4">
-                  Create +
-              </Button>
-              <UserButton/>
-          </nav>
-      </header>
-  );
+    return (
+        <header className="bg-background shadow-xxs py-4 px-6 flex justify-between items-center border-b border-gray-700 fixed top-0 left-0 w-full z-50">
+            <h1 className="text-2xl font-bold text-blue-600">MyApp</h1>
+            <nav className="space-x-10 items-center justify-center ml-20 text-lg">
+                <Link href="/" className="text-blue-600 hover:text-blue-900 hover:text-bold">Home</Link>
+                <Link href="/about" className="text-blue-600 hover:text-blue-900 hover:text-bold">About</Link>
+                <Link href="/contact" className="text-blue-600 hover:text-blue-900 hover:text-bold">Contact</Link>
+            </nav>
+            <nav className='flex items-center'>
+                <Link href="/create">
+                    <Button size="lg" className="mr-4">
+                        Create +
+                    </Button>
+                </Link>
+                <UserButton />
+            </nav>
+        </header>
+    );
 }
